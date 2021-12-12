4fced42122a8b33c9608f664ca71d2d04c12aaaf
erikmetzinfo <erik.metz.info@gmail.com>
2021-12-12 18:02:54 -0500

    only aad

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = (props) => {
-  const providers = ['twitter', 'github', 'aad'];
+  // const providers = ['twitter', 'github', 'aad'];
+  const providers = ['aad'];
   const redirect = window.location.pathname;
   const [userInfo, setUserInfo] = useState();
 
